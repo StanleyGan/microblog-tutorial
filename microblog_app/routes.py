055a290513e2cc99de68cd69b1f01a08dbf6f063
StanleyGan <glgan@sfu.ca>
2019-05-26 23:04:23 -0700

    second chapter

--- microblog_app/routes.py
+++ microblog_app/routes.py
@@ -1,6 +1,19 @@
 from microblog_app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Stanley'}
+    posts = [
+        {
+            'author': {'username':'John'},
+            'body': 'Beautiful day in Vancouver!'
+        },
+        {
+            'author': {'username':'Pikachu'},
+            'body': 'Pika pika!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
